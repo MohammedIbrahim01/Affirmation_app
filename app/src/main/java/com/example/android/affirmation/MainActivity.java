3c861ec7a363c506c065068db75db2021515e164
mohamed <@Creative-To-Death>
2018-03-15 17:58:50 +0200

    Add titleEditText

--- app/src/main/java/com/example/android/affirmation/MainActivity.java
+++ app/src/main/java/com/example/android/affirmation/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.android.affirmation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText titleEditText;
+    private String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleEditText = (EditText) findViewById(R.id.title_text_view);
     }
 }
