5f17d040cf8d4453e50a524b58fdfa0acacf2cef
mohamed <@Creative-To-Death>
2018-03-15 18:03:29 +0200

    Add affirmationEditText

--- app/src/main/java/com/example/android/affirmation/MainActivity.java
+++ app/src/main/java/com/example/android/affirmation/MainActivity.java
@@ -6,14 +6,22 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    //**** 1
     private EditText titleEditText;
     private String title;
+    //**** 2
+    private EditText affirmationEditText;
+    private String affirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //**** 1
         titleEditText = (EditText) findViewById(R.id.title_text_view);
+        //**** 2
+        affirmationEditText = (EditText) findViewById(R.id.affirmation_text_view);
+
     }
 }
