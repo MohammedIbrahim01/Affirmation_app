d1fd7c09917a922be97bd357378dc9d0956c07ff
mohamed <@Creative-To-Death>
2018-03-15 18:28:18 +0200

    Add notifyRateSpinner

--- app/src/main/java/com/example/android/affirmation/MainActivity.java
+++ app/src/main/java/com/example/android/affirmation/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.affirmation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     //**** 2
     private EditText affirmationEditText;
     private String affirmation;
+    //**** 4
+    private Spinner notifyRateSpinner;
+    private String notifyRate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         titleEditText = (EditText) findViewById(R.id.title_text_view);
         //**** 2
         affirmationEditText = (EditText) findViewById(R.id.affirmation_text_view);
+        //**** 4
+        notifyRateSpinner = (Spinner) findViewById(R.id.notify_rate_spinner);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.notify_rate_array, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        notifyRateSpinner.setAdapter(spinnerAdapter);
 
     }
 
